7d68f8ed471fa3b6d5a8eb5228d84aecb9d937e2
panda21127 <94950215+panda21127@users.noreply.github.com>
2023-10-05 21:45:37 +0000

    Add start.c

---
+++ lab3/src/start.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+    int pid = fork();
+	if ( pid == 0 ) {
+		execv( "./sequential_min_max", argv);
+	}
+    return 0;
+}
