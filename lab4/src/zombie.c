8b565483e16ac906e69a56a11676541f2fed5ee6
panda21127 <94950215+panda21127@users.noreply.github.com>
2023-10-06 14:43:02 +0000

    Добавил zombie.c

---
+++ lab4/src/zombie.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main ()
+{
+    int count=0;
+    for(int i=0;i<6;i++){
+        pid_t child_pid = fork ();
+        if (child_pid == 0) {
+            sleep (5);
+            count++;
+        }
+        else {
+            printf("GoodBye my son");
+            exit (0);
+        }
+    }
+    printf("No, father %d",count);
+    return 0;
+}
